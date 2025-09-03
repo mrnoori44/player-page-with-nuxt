d0945c78db29f4900347c1f3562c1ff9f21c4829
mrnoori44 <mohamadnr2002@gmail.com>
2025-09-03 21:28:16 +0330

    style:add yekan-akh font globally

--- app/app.vue
+++ app/app.vue
@@ -4,3 +4,6 @@
     <NuxtWelcome />
   </div>
 </template>
+<style>
+@import url('../assets/css/fonts.css');
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        yekan: ['Yekan-bakh', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
